e60bfc5b1e2d87e840efa6549fb68726d131e8b8
Konstantin Chukharev <lipen00@gmail.com>
2022-12-15 23:11:02 +0400

    Add example main

---
+++ src/main/kotlin/com/github/lipen/circuition/Main.kt
@@ -0,0 +1,31 @@
+package com.github.lipen.circuition
+
+import com.github.lipen.circuition.aig.parseAig
+import com.github.lipen.circuition.circuit.convertCircuitToDot
+import com.github.lipen.circuition.circuit.parseBench
+import okio.buffer
+import okio.sink
+import kotlin.io.path.Path
+import kotlin.io.path.createDirectories
+
+fun main() {
+    val name = "c17"
+
+    println("Name: $name")
+
+    val circuit = parseBench("data/examples/bench/c17.bench")
+    println("circuit = $circuit")
+    println("Size: ${circuit.size}")
+
+    val pathGv = "data/examples/gv/$name.gv"
+    Path(pathGv).also { it.parent.createDirectories() }.sink().buffer().useWith {
+        for (line in convertCircuitToDot(circuit)) {
+            writeln(line)
+        }
+    }
+    Runtime.getRuntime().exec("dot -Tpdf -O $pathGv").waitFor()
+
+    println()
+    val aig = parseAig("data/examples/aag/$name.aag")
+    println("aig = $aig")
+}
